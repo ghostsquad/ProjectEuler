94f4fb0d258671145f3e87acbdc4081ad25edb41
Weston McNamee <ghost.squadron@gmail.com>
2013-04-22 22:15:34 -0700

    problem5 description added

--- Problem5/Problem5/Problem5.cs
+++ Problem5/Problem5/Problem5.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
+    /*
+     * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+     * 
+     * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+     */
+
     class Problem5
     {
         static void Main(string[] args)

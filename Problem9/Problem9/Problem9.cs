1458e7c4444fb2b4f4740d97e85a67dae21c207f
ghostsquad <ghost.squadron@gmail.com>
2013-05-04 23:56:42 -0700

    started

---
+++ Problem9/Problem9/Problem9.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem9
+    {
+        private static UInt64 answer = 0;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(string.Format("answer: {0}", Problem9.answer.ToString()));
+            Console.ReadLine(); //Prevent the console from closing when we are done
+        }
+    }
+}

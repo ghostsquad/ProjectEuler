29085686df3c4c47ff8ae01fe3d9c155108de589
ghostsquad <ghost.squadron@gmail.com>
2013-05-07 21:18:30 -0700

    solved problem 10

---
+++ Problem10/Problem10/Problem10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProjectEuler 
+{
+    class Problem10
+    {
+        static void Main(string[] args)
+        {
+            ulong answer = 0;
+
+            PrimeNumbers.GeneratePrimes(1999999);
+
+            foreach (ulong prime in PrimeNumbers.KnownPrimes)
+            {
+                if (prime > 1999999)
+                {
+                    break;
+                }
+
+                answer += prime;
+            }
+
+            Console.WriteLine(string.Format("answer: {0}", answer.ToString()));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProblemX/ProblemX/ProblemX.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler 
+{
+    class ProblemX
+    {
+        static void Main(string[] args)
+        {
+            ulong answer = 0;
+
+            Console.WriteLine(string.Format("answer: {0}", answer.ToString()));
+            Console.ReadLine();
+        }
+    }
+}
